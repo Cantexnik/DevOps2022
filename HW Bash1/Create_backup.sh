2400c243ecd09d452fe5f4a582566dcc7b8361b6
Cantexnik <keizn39@gmail.com>
2022-09-19 10:14:04 +0300

    Improved backup script taking into comments

--- HW Bash1/Create_backup.sh
+++ HW Bash1/Create_backup.sh
@@ -4,34 +4,34 @@
 if [ $# -eq 3 ]
 then
 
-Src4bkp=$1
-Dst4bkp=$2
-bkpLimit=$3
+backup_source=$1
+backup_destination=$2
+backup_limit=$3
 
-    if [ -d $Dst4bkp ]; then
-        echo "Directory $Dst4bkp already exists"
-        else
-        mkdir $Dst4bkp
-        echo "Destination directory created"
+    if [ -d $backup_destination ]; then
+        echo "Directory $backup_destination already exists"
+            else
+            mkdir $backup_destination
+            echo "Destination directory created"
     fi
 
-    echo "Source: "$Src4bkp
-    echo "Destination: "$Dst4bkp
-    echo "Backup limit: "$bkpLimit
+    echo "Source: $backup_source"
+    echo "Destination: $backup_destination"
+    echo "Backup limit: $backup_limit"
 
-    bkpName=$Dst4bkp/"backup_"`date +%Y%m%d_%H%M%S_%3N`".tar"
+    bkpName=$backup_destination/"backup_"`date +%Y%m%d_%H%M%S_%3N`".tar"
 
    
     echo 
 
-    if tar cvf ${bkpName} ${Src4bkp}
+    if tar cvf ${bkpName} ${backup_source}
     then
-        bkpCount=`ls $Dst4bkp | wc -l`
+        bkpCount=`ls $backup_destination | wc -l`
 
-        if [ ${bkpCount} -gt $bkpLimit ]
+        if [ ${bkpCount} -gt $backup_limit ]
         then
-            oldBkp=`ls -1 $Dst4bkp | head -1`
-            file4remove=$Dst4bkp$oldBkp
+            oldBkp=`ls -1 $backup_destination | head -1`
+            file4remove=$backup_destination$oldBkp
             rm ${file4remove}
             echo
             echo "Backup $file4remove remove"
